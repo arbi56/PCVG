73e3d0e292941f41f72b24dce53cd84a70413939
Ron Bonner <color.ui>
2020-09-17 13:50:10 -0400

    Edit README and create setup.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PCVG
+=====
+
+The PCVG package provides the PCVG routine itself + utilities to load files, manipulate data tables, plot results and interpretPCVG groups
---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name="PCVG",
+    version="0.9.0",
+    description="Package providing OCVG and various utilities",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license="MIT License",
+    url="https://github.com/arbi56/PCVG",
+    packages=[
+        "PCVG",
+        "PCVG.utils"],
+    python_requires='>=3.5',
+    install_requires=[
+        "numpy",
+        "pandas"],
+    author='Ron Bonner',
+    author_email='ron@bonners.ca'
+)
