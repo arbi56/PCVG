3fddd7111e43ea222f00f59c2a50e7e1a7f8e4f7
Ron Bonner <color.ui>
2020-09-17 13:10:01 -0400

    Update and document

--- utils/read_mv_file.py
+++ utils/read_mv_file.py
@@ -1,19 +1,47 @@
+from collections import defaultdict
+import pandas as pd
+
 def read_mv_file(path_name, column_to_use_as_index=0, first_data_col=0, code=None, duplicate_sample_action=None):
+    """Loads a MarkerView file that was exported with the Table exporte plug-in (this correctly handes data from multiple experiments)
+        
+        The table has columns for:
+            Sample_Name
+            Sample_ID - a combination of the original file and sample name and the peaks file
+            MV_Group - the sample group assigned in MarkerView...sometimes refered to as 'Sample Type'
+            Data - each column represents a feature and the header encodes the feature values
+                    e.g 100.9308_17.84_x1 indicates m/a = 100.9308, RT = 17.84 min, in experiment 1 (the parameters for this experiment are not avilable)
+                The header may also include isotopic information determined by MarkerView during the peak finding
+                    e.g. 112.0515_1.04_x1_(Monoisotopic) or 113.0539_1.04_x1_(Isotope) - these are the only values available
+        
+        Parameters:
+        
+                path_name: path to the file in the format of the local OS)
+                column_to_use_as_index: the index number of the column that will be teh index of the data DataFrame...for MV this is column 0, the Sample_Name
+                first_data_col: the index of the first column containing data; this and subsequent columns are extracted as the data DataFrame and teh others as metaData
+                code: a code can be appended to the data; this is necessary when data sets are merged
+                duplicate_sample_action: data sets can contain damples with the same nmae, for example QCs that are injected multiple times without changing the name.
+                    This can confuse merging and plotting so can be deat with. Oprtions are "AddIndex" (add an index number corresponding to sample position to each name)
+                    or "Drop" (remove all duplicate samples)
+                    
+        Output:
+            data: a Pandas Dataframe containing only the data columns and values; index is the sample name
+            meta_data: a Pandas DataFrame containg the meta data (input columns before the first data column)
+            sample_groups: a dictionary with key=Sample_Group and value = a list of Sample_names in the group
+    """
 
     mv_data = pd.read_csv(path_name, sep='\t')
 
-    print(mv_data.head())
     data, meta_data = mv_data.copy(), None
 
     index_column = data.columns[column_to_use_as_index]  # Get name of column to use as index
 
-    # make a list of (name, index) tuples; we also strip any leading or trailing spaces
+    # Find out if there are duplicate names...
+    # make a dictionary of name:[index] and count how many stored at each key; we also strip any leading or trailing spaces
+    
     names = [(x[1].strip (), x[0]) for x in data.itertuples()]   # Sample Name, index (an integer)...strip removes any spaces
 
-    data.head()
-    # Find out if there are duplicate names...
-    # make a dictionary of name:[index] and count how many stored at each key
     name_dict = defaultdict(list)
+    
     for n in names:
         name_dict[n[0]] += [n[1]]  # {name: [row indices]}
 
@@ -25,19 +53,24 @@ def read_mv_file(path_name, column_to_use_as_index=0, first_data_col=0, code=Non
             if len(name_dict[k]) > 1:
                 drop_list += name_dict[k]
 
-        if len(drop_list) > 0:
+        if drop_list:
             print ("Dropping duplicate samples")
-        data = data.drop(drop_list, axis='index')
+            data = data.drop(drop_list, axis='index')
+            # since we removed some samples, we check for empty columns, since 0 is False
+            #data.any() will be True for any columns that have at least one non-zero entry
+            data = data.loc[:, data.any()] 
 
     elif duplicate_sample_action == 'AddIndex':
 
         for k in name_dict:
+            
             if len(name_dict[k]) > 1:
+                
                 for i, n in enumerate(name_dict[k]):     # loop the (name,index) tuples, make a new name by combining and restore
-                    #desc = "{}#{}".format(data.iloc[n, column_to_use_as_index], i)
-                    desc = f"{data.iloc[n, column_to_use_as_index]}#{i:02}"       #format index as a 2 character string padded with zeros
-                    data.iloc[n, column_to_use_as_index] = desc
-
+                    
+                    data.iloc[n, column_to_use_as_index] = f"{data.iloc[n, column_to_use_as_index]}#{i:02}"       #format as name + index number as a 2 character string padded with zeros                
+    
+    #make the name column the DataFrame index
     data.set_index(index_column, inplace=True)
 
     # If first_data_col is True (i.e. > 0) split the frame in two pieces..the index of each will be the same
@@ -53,4 +86,3 @@ def read_mv_file(path_name, column_to_use_as_index=0, first_data_col=0, code=Non
     sample_groups = meta_data.groupby('MV_Group').apply(lambda x: x.index.tolist())
 
     return data, meta_data, sample_groups
-    
