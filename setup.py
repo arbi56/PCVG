39590b73a1568bc82fe5dac1872dfd1b54c79efd
Ron Bonner <color.ui>
2020-09-18 08:29:13 -0400

    Specify package list explicitly (no find_packages)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type='text/markdown',
     license="MIT License",
     url="https://github.com/arbi56/PCVG",
-    packages=find_packages(),
+    packages=['pcvg'],
     python_requires='>=3.5',
     install_requires=[
         "numpy",
