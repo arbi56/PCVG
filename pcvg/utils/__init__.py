6d8b93969108eba5439b677718d5b110ce54d9d8
Ron Bonner <color.ui>
2020-09-27 07:04:15 -0400

    Update utils __init__.py

--- pcvg/utils/__init__.py
+++ pcvg/utils/__init__.py
@@ -1,5 +1,7 @@
 from .read_mv_file import read_mv_file
 from .who_am_I import who_am_I
+from .extract_features import extract_features
 
 
-__all__ = ["read_mv_file", "who_am_I"]
+__all__ = ["read_mv_file", "who_am_I", "extract_features"]
+
