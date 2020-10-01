e836abe664d59287e9532098ee394653c540ce6f
Ron Bonner <color.ui>
2020-10-01 08:01:40 -0400

    Add plot_raw_data; update __init__.py

--- pcvg/plotting/__init__.py
+++ pcvg/plotting/__init__.py
@@ -1,4 +1,5 @@
 #__init__.py
 from .set_size_column import set_size_column
+from .plot_raw_data import plot_raw_data
 
-__all__ = ["set_size_column"]
+__all__ = ["set_size_column", "plot_raw_data"]
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def readme():
