c4b2ea850f6337d1860f2d7adb4dc5643c45b41a
Ron Bonner <color.ui>
2020-10-22 07:14:02 -0400

    Update plotting/__init__.py

--- pcvg/plotting/__init__.py
+++ pcvg/plotting/__init__.py
@@ -3,5 +3,6 @@ from .set_size_column import set_size_column
 from .plot_raw_data import plot_raw_data
 from .js_select import js_select
 from .get_tool_tips_format import get_tool_tips_format
+from .add_plot_selection_tools import add_plot_selection_tools
 
-__all__ = ["set_size_column", "plot_raw_data", "js_select", "get_tool_tips_format"]
+__all__ = ["set_size_column", "plot_raw_data", "js_select", "get_tool_tips_format", "add_plot_selction_tools"]
